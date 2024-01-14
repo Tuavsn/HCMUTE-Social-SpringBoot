b7d585019069773fe3a4167bb42d23217bf8cdb2
Tuavsn <43782459+Tuavsn@users.noreply.github.com>
2024-01-14 12:11:54 +0700

    Add files via upload

---
+++ src/main/java/Tuan/HCMUTESocial/HcmuteSocialApplication.java
@@ -0,0 +1,13 @@
+package Tuan.HCMUTESocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcmuteSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcmuteSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tuan/HCMUTESocial/HcmuteSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package Tuan.HCMUTESocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcmuteSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
